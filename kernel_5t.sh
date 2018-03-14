8c4775f50b9135037749b07cca66ff199a873f83
mj1687 <alessandro.pasini12@gmail.com>
2018-03-14 15:44:06 +0100

    complete the script
    Change-Id: I98c049ce1fa1015902b17dfb449d5d9b64042b01
    Signed-off-by: mj1687 <alessandro.pasini12@gmail.com>

--- kernel_5t.sh
+++ kernel_5t.sh
@@ -2,14 +2,20 @@
 
 clear
 echo "Compilazione avviata"
-export CROSS_COMPILE=gcc-aarch64/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/Sorgenti/kernels/5t/gcc-aarch64/bin/aarch64-linux-android-
 export ARCH=arm64
-export DEFCONFIG=oneplus5_defconfig
 
 cd source_5t
 make clean
 make mrproper
-cd ..
-rm -rf out
-mkdir out
-make source_5t 0=out ARCH DEFCONFIG -j8
+make oneplus5_defconfig
+rm -rf ../out
+mkdir ../out
+make 0=../out ARCH=arm64 -j8
+
+#cd wlan && make clean && cd ..
+
+#make -C kernel/ M=../wlan/ O=../out/ modules WLAN_ROOT=../wlan/ MODNAME=wlan BOARD_PLATFORM=msm8976 CONFIG_PRONTO_WLAN=m
+
+echo Done!
+
